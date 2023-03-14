e3adf76a79e05baf834462a2756c9befb9d240b5
joao.cordeiro <joaocordeiro2134@gmail.com>
2023-03-13 22:37:34 -0300

    aula finish

--- aulaDB/src/main/java/br/com/auladb/www/AulaDB.java
+++ aulaDB/src/main/java/br/com/auladb/www/AulaDB.java
@@ -1,5 +1,12 @@
 package br.com.auladb.www;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Cordeiro
@@ -7,6 +14,26 @@ package br.com.auladb.www;
 public class AulaDB {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        try {
+            // Subir o driver para oso - MYSQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // Tentar conectar
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/aula_database", 
+                    "root", 
+                    "1234"
+            );            
+            
+            //Inserção no banco            
+            Statement comand = conn.createStatement();
+            comand.executeUpdate(
+                    "INSERT INTO Aluno (nome, ra) VALUES ('João Victor Cordeiro', '27099-5')"
+            );
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(AulaDB.class.getName()).log(Level.SEVERE, null, ex);        
+        }
     }
 }
